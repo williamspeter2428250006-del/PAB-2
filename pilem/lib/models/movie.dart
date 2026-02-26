97d63655cb64472d925a944b812a369ceae058f1
WilliamsPeter <williamspeter_2428250006@mhs.mdp.ac.id>
2026-02-26 09:23:17 +0700

    push
    2

--- pilem/lib/models/movie.dart
+++ pilem/lib/models/movie.dart
@@ -16,4 +16,16 @@ class Movie {
     required this.releaseDate,
     required this.voteAverage,
   });
+
+   factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average'].toDouble(),
+    );
+  }
 }
