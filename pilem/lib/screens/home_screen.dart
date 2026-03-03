65cf3517c25345deaefcd2aae8c65ee15d2f46f0
williamspeter2428250006-del <williamspeter_2428250006@mhs.mdp.ac.id>
2026-03-03 11:41:44 +0700

    p
    push

---
+++ pilem/lib/screens/home_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:pilem/models/movie.dart';
+import 'package:pilem/services/api_services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiServices _apiServices = ApiServices();
+
+  List<Movie> _allMovies = [];
+  List<Movie> _trendingMovies = [];
+  List<Movie> _popularMovies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
+
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData = await _apiServices
+        .getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData = await _apiServices
+        .getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData = await _apiServices
+        .getPopularMovies();
+
+    setState(() {
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovies = trendingMoviesData
+          .map((e) => Movie.fromJson(e))
+          .toList();
+      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pilem')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildMovieList('All Movie', _allMovies),
+            _buildMovieList('Trending Movie', _trendingMovies),
+            _buildMovieList('Popular Movie', _popularMovies),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMovieList(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final Movie movie = movies[index];
+              return Column(
+                children: [
+                  Image.network(
+                    'https://image.tmdb.org/t/p/w500/${movie.posterPath}',
+                    height: 150,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    movie.title.length > 14
+                        ? '${movie.title.substring(0, 10)}...'
+                        : movie.title,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- pilem/lib/services/api_services.dart
+++ pilem/lib/services/api_services.dart
@@ -1,14 +1,25 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class ApiServices {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey =
-      "570ab6b34423960e3129dfbf45be11da&status=1&mode=0";
-  Future<List<Map<String, dynamic>>> getAllMovie() async {
+  static const String apiKey = "570ab6b34423960e3129dfbf45be11da";
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response = await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);    
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);    
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);    
   }
 }
